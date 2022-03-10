7fdb7f2ccac4254b2e0241608da0dd0e1f866733
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-03-10 22:43:17 +0530

    Add files via upload

---
+++ loop11.py
@@ -0,0 +1,14 @@
+i=1
+b=1
+while i<=5:
+    a=1
+    while a<=4-i:
+        print('',end='')
+        a=a+1
+    j=1
+    while j<=b:
+        print(i,end='')
+        j=j+1
+    print()
+    b=b+2
+    i=i+1
